ba4868220cc7e03c063cee93e82af7bf3199815a
Naveen_2023 <119423153+naveenprohack@users.noreply.github.com>
2024-05-28 08:43:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,430 @@
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <link rel="icon" href="images/logo.png" type="image/gif" />
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title> Sivaji Yoga Foundation </title>
+
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet" />
+
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+  <!-- owl carousel -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha256-UhQQ4fxEeABh4JrcmAJ1+16id/1dnlOEVCFOxDef9Lw=" crossorigin="anonymous" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+
+<body>
+   <div class="loader_bg">
+        <div class="loader"><img src="images/loading.gif" alt="" /></div>
+    </div>
+    <?php
+include('header.php'); 
+?>
+    <!-- end header section -->
+    <!-- slider section -->
+    <section class="slider_section ">
+      <div class="slider_bg_box">
+        <img src="images/samiji_banner_.jpg" alt="sami ji">
+      </div>
+      <div id="customCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7 mx-auto">
+                  <div class="detail-box">
+                    <h1>
+                      Yoga & Meditation
+                    </h1>
+                    <p>
+                      Discover the harmonious blend of physical and mental practices designed to foster inner peace and holistic well-being. Start your journey towards a balanced lifestyle with our expert-guided yoga and meditation sessions.                    </p>
+                    <a href="about.php" class="btn">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7 mx-auto">
+                  <div class="detail-box">
+                    <h1>
+                      Mindfulness & Inner Peace
+                    </h1>
+                    <p>
+                      Learn the art of mindfulness to live in the present moment and find inner tranquility. Our guided meditation sessions and mindfulness exercises are designed to help you reduce stress, enhance self-awareness, and achieve a profound sense of peace and well-being.                    </p>
+                    <a href="about.php" class="btn">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container ">
+              <div class="row">
+                <div class="col-md-7 mx-auto">
+                  <div class="detail-box">
+                    <h1>
+                      Spiritual Awakening
+                    </h1>
+                    <p>
+                      Embark on a journey of self-discovery and enlightenment. Our app offers resources and practices to help you connect with your higher self, cultivate inner peace, and achieve spiritual awakening through yoga and meditation.                    </p>
+                    <a href="about.php" class="btn">
+                      Read More
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ol class="carousel-indicators">
+          <li data-target="#customCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#customCarousel" data-slide-to="1"></li>
+          <li data-target="#customCarousel" data-slide-to="2"></li>
+        </ol>
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+
+
+
+  <!-- about section -->
+
+  <section class="about_section layout_padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="img-box">
+            <img src="images/logo.png" alt="" />
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="detail-box">
+            <div class="heading_container ">
+              <h2>
+                About Us
+              </h2>
+            </div>
+            <p>
+              As a revered Guru Ji, I guide individuals on their spiritual journeys, helping them discover inner peace and enlightenment. With profound wisdom and compassion, I offer insights and practices that awaken the soul and nurture a deep connection with the divine.            </p>
+            <a href="about.php" class="btn">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end about section -->
+
+  <!-- asan section -->
+
+  <section class="asan_section layout_padding-bottom">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Basic Yoga Asanas
+        </h2>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-4 col-lg-3 ">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/y01.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Yoga Asana 1
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots making it over 2000 years old.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 ">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/y02.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Yoga Asana 2
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots making it over 2000 years old.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 ">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/y03.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Yoga Asana 3
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots making it over 2000 years old.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 ">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/y04.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Yoga Asana 4
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots making it over 2000 years old.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 ">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/y05.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Yoga Asana 5
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots making it over 2000 years old.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 ">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/y06.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Yoga Asana 6
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots making it over 2000 years old.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 ">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/y07.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Yoga Asana 7
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots making it over 2000 years old.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 col-lg-3 ">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/y08.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                Yoga Asana 8
+              </h5>
+              <p>
+                Lorem Ipsum is not simply random text. It has roots making it over 2000 years old.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="btn-box">
+        <a href="" class="btn">
+          See More
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- end asan section -->
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <div class="quote_container">
+          <div class="bg-box">
+            <video id="myVideo">
+              <source src="images/video_01.mp4" type="video/mp4">
+              <source src="video.webm" type="video/webm">
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div class="container-fluid">
+            <div class="quote_box">
+              <h2 class="h_qoute">
+                Explore the yoga of life.
+              </h2>
+              <div class="play_btn">
+                <button id="playPauseBtn">
+                  <i class="fa fa-play" aria-hidden="true"></i>
+                </button>
+              </div>
+              <p class="col-md-8 mx-auto px-0 p_qoute">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nihil nam quisquam? Eligendi enim est quasi quibusdam sed praesentium beatae.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-5 d-none d-md-block">
+        <div class="social-media-icons">
+          <div class="social-icon">
+            <i class="fab fa-whatsapp"></i>
+            <p>Chat with me on WhatsApp</p>
+          </div>
+          <div class="social-icon">
+            <i class="fab fa-youtube"></i>
+            <p>Watch our videos on YouTube</p>
+          </div>
+          <div class="social-icon">
+            <i class="fab fa-facebook"></i>
+            <p>Follow us on Facebook</p>
+          </div>
+          <div class="social-icon">
+            <i class="fab fa-instagram"></i>
+            <p>Follow us on Instagram</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- client section -->
+
+  <section class="client_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          Testimonial
+        </h2>
+      </div>
+      <div class="carousel-wrap ">
+        <div class="owl-carousel client_owl-carousel">
+          <div class="item">
+            <div class="box">
+              <div class="img-box">
+                <i class="fa fa-quote-right" aria-hidden="true"></i>
+              </div>
+              <div class="detail_container">
+                <div class="detail-box">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                  </p>
+                  <h6 class="rating">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </h6>
+                </div>
+                <h5>
+                  Maya Maxwell
+                </h5>
+              </div>
+            </div>
+          </div>
+          <div class="item">
+            <div class="box">
+              <div class="img-box">
+                <i class="fa fa-quote-right" aria-hidden="true"></i>
+              </div>
+              <div class="detail_container">
+                <div class="detail-box">
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                  </p>
+                  <h6 class="rating">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </h6>
+                </div>
+                <h5>
+                  Ruby Jonas
+                </h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end client section -->
+
+ 
+
+
+<?php include('contact.php')?>
+<?php include('footer.php')?>
+
+  <!-- jQery -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.js"></script>
+  <!-- owl carousel -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+  <script src="js/index.js"></script>
+  <!-- Google Map -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap"></script>
+  <!-- End Google Map -->
+
+</body>
+
+</html>
